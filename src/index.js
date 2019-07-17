94ada1d22d153fdd71bc4f851d67bb7bded7b116
shivanigangadharan <shivani.gangadharan@gmail.com>
2019-07-17 16:33:04 +0530

    service worker registered

--- src/index.js
+++ src/index.js
@@ -15,4 +15,22 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
+
+// const check = () => {
+//     if (!('serviceWorker' in navigator)) {
+//         throw new Error('No Service Worker support!')
+//     }
+//     if (!('PushManager' in window)) {
+//         throw new Error('No Push API Support!')
+//     }
+// }
+
+// check();
+
+// const registerServiceWorker = async () => {
+//     const swRegistration = await navigator.serviceWorker.register(
+//         'service.js'
+//     );
+//     return swRegistration;
+// }
