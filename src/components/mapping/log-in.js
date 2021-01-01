54bda06ec1a5f11eb26444496d0e8327ab4b24be
shivanigangadharan <shivani.gangadharan@gmail.com>
2021-01-01 23:48:34 +0530

    aligned login component

--- src/components/mapping/log-in.js
+++ src/components/mapping/log-in.js
@@ -53,51 +53,53 @@ function NormalLoginForm(props) {
     return (
         <StyledLogin>
             <BoxDiv>
-                <Heading> LOG IN </Heading>
-                <Form onSubmit={e => { handleSubmit(e) }} className="login-form">
-                    <Form.Item>
-                        {getFieldDecorator('email', {
-                            rules: [{ required: true, message: 'Please input your username!' }],
-                        })(
-                            <Input
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="Username"
-                            />,
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('password', {
-                            rules: [{ required: true, message: 'Please input your Password!' }],
-                        })(
-                            <Input
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type="password"
-                                placeholder="Password"
-                            />,
-                        )}
-                    </Form.Item>
-                    <Form.Item>
-                        {getFieldDecorator('remember', {
-                            valuePropName: 'checked',
-                            initialValue: true,
-                        })(<Checkbox>Remember me</Checkbox>)}
-                        <a className="login-form-forgot" href="">
-                            Forgot password
+                <center>
+                    <Heading> LOG IN </Heading>
+                    <Form onSubmit={e => { handleSubmit(e) }} className="login-form">
+                        <Form.Item>
+                            {getFieldDecorator('email', {
+                                rules: [{ required: true, message: 'Please input your username!' }],
+                            })(
+                                <Input
+                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    placeholder="Username"
+                                />,
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('password', {
+                                rules: [{ required: true, message: 'Please input your Password!' }],
+                            })(
+                                <Input
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    type="password"
+                                    placeholder="Password"
+                                />,
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('remember', {
+                                valuePropName: 'checked',
+                                initialValue: true,
+                            })(<Checkbox>Remember me</Checkbox>)}
+                            <a className="login-form-forgot" href="">
+                                Forgot password
             </a>
 
-                        {/* <Link to="/mobile/landingpage"> */}
-                        <Button type="primary" htmlType="submit" className="login-form-button">
-                            Log in
-                                </Button>
-                        {/* </Link> */}
-                        <Link to="/mobile/landingpage">
+                            {/* <Link to="/mobile/landingpage"> */}
                             <Button type="primary" htmlType="submit" className="login-form-button">
-                                Move to next page
+                                Log in
                                 </Button>
-                        </Link>
-                        Or <a href="">register now!</a>
-                    </Form.Item>
-                </Form>
+                            {/* </Link> */}
+                            <Link to="/mobile/landingpage">
+                                <Button type="primary" htmlType="submit" className="login-form-button">
+                                    Move to next page
+                                </Button>
+                            </Link>
+                            Or <a href="">Register now!</a>
+                        </Form.Item>
+                    </Form>
+                </center>
             </BoxDiv>
         </StyledLogin>
     );
